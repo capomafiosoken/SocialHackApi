2dac471e254ad5fe02bb39c75d8afa7f73677818
g.kenjetaev <capomafiosoken@gmail.com>
2020-07-18 13:16:02 +0600

    comments

--- Controllers/SocialHackController.cs
+++ Controllers/SocialHackController.cs
@@ -19,37 +19,51 @@ namespace SocialHackApi.Controllers
             _service = service;
         }
 
+        /// <summary>
+        /// Shows a list of city objects and sorts by weight.
+        /// </summary>
         [HttpGet("cityObjects")]
         public async Task<IEnumerable<CityObject>> GetCityObjects()
         {
             return await _service.LoadObjectsByWeight();
         }
-        
+        /// <summary>
+        /// Shows a list of tasks and sorts by weight.
+        /// </summary>
         [HttpGet("cityTasks")]
         public async Task<IEnumerable<CityTask>> GetCityTasks()
         {
             return await _service.LoadCityTasks();
         }
-        
+        /// <summary>
+        /// Shows a list of feedbacks and sorts by weight.
+        /// </summary>
         [HttpGet("objectFeedbacks")]
         public async Task<IEnumerable<ObjectFeedback>> GetObjectFeedbacks()
         {
             return await _service.LoadFeedbacks();
         }
-        
+        /// <summary>
+        /// Shows a list of districts in city with overall disabledfriendly score about.
+        /// </summary>
         [HttpGet("districts")]
         public async Task<IEnumerable<District>> GetDistricts()
         {
             return await _service.LoadDistricts();
         }
         
-        
+        /// <summary>
+        /// Shows a list of disabledfriendly score of city object.
+        /// </summary> 
         [HttpGet("objectScores/{id}")]
         public async Task<IEnumerable<ObjectScore>> GetObjectScores(long id)
         {
             return await _service.LoadObjectScores(id);
         }
 
+        /// <summary>
+        /// Create a feedback.
+        /// </summary> 
         [HttpPost("feedback/create")]
         public async Task<IActionResult> CreateFeedback([FromBody] ObjectFeedback objectFeedback)
         {
@@ -57,20 +71,27 @@ namespace SocialHackApi.Controllers
             return Ok();
         }
        
+        /// <summary>
+        /// Create a score.
+        /// </summary> 
         [HttpPost("score/create")]
         public async Task<IActionResult> CreateScore([FromBody] ObjectScore objectScore)
         {
             await _service.AddScore(objectScore);
             return Ok();
         }
-        
+        /// <summary>
+        /// Create a task.
+        /// </summary>  
         [HttpPost("task/create")]
         public async Task<IActionResult> CreateTask([FromBody] CityTask cityTask)
         {
             await _service.AddTask(cityTask);
             return Ok();
         }
-        
+        /// <summary>
+        /// Updates a task.
+        /// </summary>   
         [HttpPost("task/update")]
         public async Task<IActionResult> UpdateTask([FromBody] CityTask cityTask)
         {
